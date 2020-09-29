822eed62117fc37be075d6533fcc0c552951ace1
0x00 <seq@localhost.localdomain>
2020-09-29 18:22:57 +0300

    Extract ALL the data

--- cron.php
+++ cron.php
@@ -37,31 +37,36 @@ foreach ($cookies as $key => $val) {
     $cookieStr .= $key . '=' . $val . '; ';
 }
 
-if (1 === 1) {
-    $curl       = curl_init();
-    $params     = ['rn' => 'MK2248'];
-    
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_URL, E_CSDD . 'tadati/');
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Cookie: ' . $cookieStr));
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+for ($i=65; $i<=90; $i++) {
+    $L1=chr($i);
+    for ($j=65; $j<=90; $j++) {
+        $L2=chr($j);
+        for ($x=1; $x<=9999; $x++) {
+            $VNZ = $L1.$L2.$x;
+            $curl       = curl_init();
+            $params     = ['rn' => $VNZ];
+            
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_URL, E_CSDD . 'tadati/');
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Cookie: ' . $cookieStr));
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+        
+            $response = curl_exec($curl);
 
-    $response = curl_exec($curl);
-    $dom = new Dom;
-    $dom->setOptions(
-        (new Options())
-            ->cleanupInput(true)
-    );
-    $dom->loadStr($response);
-    $tableText = $dom->find('#refer-table');
-
-    foreach ($tableText as $i => $data) {
-        $fullData .= $data->innerHtml;
+            $dom = new Dom;
+            $dom->loadStr($response);
+            
+            $tableText = $dom->find('#refer-table');
+        
+            foreach ($tableText as $i => $data) {
+                $fullData .= $data->innerHtml;
+            }
+        
+            $db->insert('vehicle_numbers', [
+                'vnz'               => $VNZ,
+                'data_original'     => $fullData
+            ]);
+        }
     }
-
-    $db->insert('vehicle_numbers', [
-        'vnz'               => 'MK2248',
-        'data_original'     => $fullData
-    ]);
 }
