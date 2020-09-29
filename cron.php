a4c8ee05338189cad4b20dbef7e24f1ea70b6892
0x00 <seq@localhost.localdomain>
2020-09-29 16:30:09 +0300

    Get headers

--- cron.php
+++ cron.php
@@ -4,6 +4,7 @@ require_once 'library/constants.php';
 
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, 1);
 
 $params = array(
     'email'	=> E_CSDD_LOGIN,
@@ -15,9 +16,14 @@ curl_setopt($curl, CURLOPT_URL, E_CSDD . 'login/?action=doLogin');
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
 
-$result = curl_exec($curl);
+$response = curl_exec($curl);
 
-var_dump($result);
+$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+$header = substr($response, 0, $header_size);
+$body = substr($response, $header_size);
+
+var_dump($header);
+var_dump($body);
 
 if (1 === 2) {
     $curl = curl_init();
